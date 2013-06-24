8be94e90efd022668aa28628af70c38ddddf1a42
forrest <forrest@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-06-24 15:04:53 +0000

    Updated project web page with more information, links and sample image.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/refnet@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,7 +43,20 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
+<p>This is the <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/">refnet</a> package for the <a href="http://www.r-project.org/">R statistical computing environment</a>. Our goal is to provide a package of tools for bibliometric analyses. This package and its tools are currently designed to read, analyze and visualize <a href="http://apps.webofknowledge.com/">Thomson-Reuters Web of Knowledge/Science, ISI</a> and <a href="http://www.scopus.com/home.url">SCOPUS</a> format reference data files. Social network analyses, geocoding of addresses and spatial visualization are supported.</p>
+
+<img src="refnet_sample_output.png" alt="refnet sample output for publications from the UF Geography Department, 2010-2013" width="742" height="370" />
+
+<p>We consider this work currently pre-alpha, however, as we progress we will continue to release source code and updates via <a href="http://r-forge.r-project.org/">R-Forge</a>.  If you would like more information, help or have comments regarding the code and package development please don't hesitate to email or <a href="http://www.clas.ufl.edu/users/forrest/">contact us</a>.
+
+<p>
+	<a href="http://www.clas.ufl.edu/users/forrest/">Forrest R. Stevens</a><br />
+	Package Administrator<br />
+	Ph.D. Candidate<br />
+	Land Use and Environmental Change Institute (LUECI)<br />
+	<a href="http://www.geog.ufl.edu/">Department of Geography, University of Florida</a><br />
+</p>
+
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
